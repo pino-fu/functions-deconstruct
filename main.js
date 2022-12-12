2b8d7a5c6d917a52960eb1e0c30d0f596d56cb13
Ben Hoberg <benjhoberg@gmail.com>
2022-12-12 10:19:48 -0600

    JS challenge involving simple function declaration and invocations.

---
+++ main.js
@@ -0,0 +1,30 @@
+const callCustomer = () => {
+    console.log("Call customer to verify delivery.")
+}
+
+const callShipper = () => {
+    console.log("Call shipper to verify pickup.")
+}
+
+const getFuel = () => {
+    console.log("Get some diesel!")
+}
+
+const getLoaded = () => {
+    console.log("Physically load the truck.")
+}
+
+const makeDelivery = () => {
+    console.log("Time to physicall make the delivery!")
+}
+
+const getLunch = () => {
+    console.log("Time for lunch!")
+}
+
+getFuel();
+callShipper();
+callCustomer();
+getLoaded();
+makeDelivery();
+getLunch();
